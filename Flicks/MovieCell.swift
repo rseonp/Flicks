778eb51d2931f74e836676c318e45e25cd31f652
rseonp <Victor@victors-mbp.resnet.wustl.edu>
2016-01-22 18:57:37 -0600

    Finished QuickStart

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Victor Li Wang on 1/22/16.
+//  Copyright © 2016 Victor Li Wang. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
